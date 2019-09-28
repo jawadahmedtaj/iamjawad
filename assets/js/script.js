17ce43715054bd7277681fac35a678cad5176ad1
Jawad Ahmed <jawadahmedtaj@gmail.com>
2019-09-28 21:12:48 +0200

    added jquery, some hover effects

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,14 @@ $(function () {
         $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
     });
 })
+
+$(".card").hover(function () {
+    $(this).toggleClass("skillsItemsBorder");
+})
+
+$(".contactBox").hover(function () {
+    $(this).toggleClass("contactBoxColor");
+    $(".contactHeading", this).toggleClass("contactBoxColor");
+    $(".contentText", this).toggleClass("contactBoxColor");
+    $(".contactImgs", this).fadeToggle(0);
+})
