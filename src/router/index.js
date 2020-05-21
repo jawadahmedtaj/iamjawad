e4ce197810fd0e73819a84b9a81a668059e475c3
Jawad Ahmed <jawadahmedtaj@gmail.com>
2020-05-21 17:40:49 +0200

    vuetify added

--- src/router/index.js
+++ src/router/index.js
@@ -8,14 +8,14 @@ const routes = [
   {
     path: "/",
     name: "HelloWorld",
-    component: HelloWorld,
-  },
+    component: HelloWorld
+  }
 ];
 
 const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
-  routes,
+  routes
 });
 
 export default router;
