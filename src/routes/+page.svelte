79b23a18f3903aa3f61885a5d51daacfc0220c0a
Jawad Ahmed <jawadahmedtaj@gmail.com>
2023-09-20 22:49:32 +0500

    Updated

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,7 @@
 	import SplitType from 'split-type';
 	import { onMount } from 'svelte';
 	import { animate, stagger, scroll, inView } from 'motion';
+	import { Tooltip } from '@svelte-plugins/tooltips';
 
 	onMount(() => {
 		const text = new SplitType('.mainHeadings');
@@ -57,22 +58,48 @@
 	});
 
 	const technologyIcons = {
-		vue: 'logos:vue',
-		github: 'mdi:github',
-		netlify: 'logos:netlify',
-		svelte: 'devicon:svelte',
-		tailwindcss: 'devicon:tailwindcss',
-		react: 'devicon:react',
-		vueuse: 'logos:vueuse',
-		firebase: 'logos:firebase',
-		nuxt: 'logos:nuxt-icon',
-		css: 'logos:css-3',
-		html: 'logos:html-5',
-		js: 'logos:javascript',
-		pwa: 'logos:pwa'
+		vue: { icon: 'logos:vue', title: 'Vue 2/3' },
+		github: { icon: 'mdi:github', title: 'Github' },
+		netlify: { icon: 'logos:netlify', title: 'Netlify' },
+		svelte: { icon: 'devicon:svelte', title: 'Svelte/SvelteKit' },
+		tailwindcss: { icon: 'devicon:tailwindcss', title: 'Tailwindcss' },
+		react: { icon: 'devicon:react', title: 'React' },
+		vueuse: { icon: 'logos:vueuse', title: 'Vueuse' },
+		firebase: { icon: 'logos:firebase', title: 'Firebase' },
+		nuxt: { icon: 'logos:nuxt-icon', title: 'Nuxt 3' },
+		css: { icon: 'logos:css-3', title: 'CSS' },
+		html: { icon: 'logos:html-5', title: 'HTML' },
+		js: { icon: 'logos:javascript', title: 'Javascript' },
+		pwa: { icon: 'logos:pwa', title: 'PWA - Progressive Web App' },
+		vuetify: { icon: 'logos:vuetifyjs', title: 'Vuetify' },
+		antDesign: { icon: 'logos:ant-design', title: 'Ant Design' },
+		pinia: { icon: 'logos:pinia', title: 'Pinia' },
+		naive: { icon: 'logos:naiveui', title: 'Naive UI' }
 	};
 
 	const projects = [
+		{
+			title: 'C3-CRM',
+			image: '/images/C3CRM.png',
+			description: {
+				en: 'Official CRM solution of Thomasgold solutions. The CRM is built using our own custom Corteza Low Code Platform based implementation. However calendar is made separately using Nuxt 3, Pinia, Tailwindcss, Vueuse, Ant design, and Vuetify',
+				de: ''
+			},
+			technologyStack: [
+				'html',
+				'css',
+				'js',
+				'vue',
+				'nuxt',
+				'pinia',
+				'tailwindcss',
+				'vueuse',
+				'antDesign',
+				'vuetify'
+			],
+			deployment: { link: '', icon: '' },
+			codeLink: { link: '', icon: '' }
+		},
 		{
 			title: 'Introspectify',
 			image: '/images/Introspectify.png',
@@ -80,7 +107,7 @@
 				en: 'A productivity app that helps you track your time and stay focused on your goals. All data is stored locally, no backend.',
 				de: ''
 			},
-			technologyStack: ['svelte', 'html', 'css', 'js', 'tailwindcss', 'pwa'],
+			technologyStack: ['html', 'css', 'js', 'svelte', 'tailwindcss', 'pwa'],
 			deployment: { link: 'https://introspectify.netlify.app/', icon: 'netlify' },
 			codeLink: { link: 'https://github.com/jawadahmedtaj/introspectify', icon: 'github' }
 		},
@@ -91,7 +118,19 @@
 				en: 'A simple chat application made using Vue 3/Nuxt 3 with firebase integration',
 				de: ''
 			},
-			technologyStack: ['vue', 'nuxt', 'html', 'css', 'js', 'tailwindcss', 'vueuse', 'firebase', 'pwa'],
+			technologyStack: [
+				'html',
+				'css',
+				'js',
+				'vue',
+				'nuxt',
+				'naive',
+				'pinia',
+				'tailwindcss',
+				'vueuse',
+				'firebase',
+				'pwa'
+			],
 			deployment: { link: 'https://chat-app-jawad.netlify.app/', icon: 'netlify' },
 			codeLink: { link: 'https://github.com/jawadahmedtaj/chat-app-nuxt', icon: 'github' }
 		},
@@ -102,7 +141,7 @@
 				en: 'An app that fetches dad jokes from an API and displays them. Made using React.',
 				de: ''
 			},
-			technologyStack: ['react', 'html', 'css', 'js'],
+			technologyStack: ['html', 'css', 'js', 'react'],
 			deployment: { link: 'https://dadjokesapp-jawad.netlify.app', icon: 'netlify' },
 			codeLink: { link: 'https://github.com/jawadahmedtaj/React-Dad-Jokes', icon: 'github' }
 		}
@@ -117,7 +156,7 @@
 	</div>
 </div>
 
-<div class="container grid h-screen mx-auto place-items-center sectionContainer" id="work">
+<div class="container grid pb-5 mx-auto place-items-center sectionContainer" id="work">
 	{#each projects as { title, image, description, deployment, codeLink, technologyStack }, idx (idx)}
 		{#if idx % 2 === 0}
 			<div class="grid grid-flow-col grid-cols-2 projectDetail" class:mt-4={idx > 0}>
@@ -128,17 +167,26 @@
 					<div class="self-center h2">{title}</div>
 					<div class="self-center pt-2 h4">{description.en}</div>
 					<div class="grid justify-start grid-flow-col pt-2 text-2xl">
-						<a href={deployment.link} target="_blank" class="btn variant-filled-primary">
-							<iconify-icon icon={technologyIcons[deployment.icon]} />
-						</a>
-						<a href={codeLink.link} target="_blank" class="ml-2 btn variant-filled-primary">
-							<iconify-icon icon={technologyIcons[codeLink.icon]} />
-						</a>
+						{#if deployment.link}
+							<Tooltip content={technologyIcons[deployment.icon].title}>
+								<a href={deployment.link} target="_blank" class="btn variant-filled-primary">
+									<iconify-icon icon={technologyIcons[deployment.icon].icon} />
+								</a>
+							</Tooltip>
+						{/if}
+						{#if codeLink.link}
+							<Tooltip content={technologyIcons[codeLink.icon].title}>
+								<a href={codeLink.link} target="_blank" class="ml-2 btn variant-filled-primary">
+									<iconify-icon icon={technologyIcons[codeLink.icon].icon} />
+								</a>
+							</Tooltip>
+						{/if}
 					</div>
 					<div class="grid items-center justify-start grid-flow-col gap-1">
-						<p>Stack:</p>
 						{#each technologyStack as icon}
-							<iconify-icon icon={technologyIcons[icon]} />
+							<Tooltip content={technologyIcons[icon].title}>
+								<iconify-icon icon={technologyIcons[icon].icon} />
+							</Tooltip>
 						{/each}
 					</div>
 				</div>
@@ -149,17 +197,26 @@
 					<div class="self-center h2">{title}</div>
 					<div class="self-center pt-2 h4">{description.en}</div>
 					<div class="grid justify-start grid-flow-col pt-2 text-2xl">
-						<a href={deployment.link} target="_blank" class="btn variant-filled-primary">
-							<iconify-icon icon={technologyIcons[deployment.icon]} />
-						</a>
-						<a href={codeLink.link} target="_blank" class="ml-2 btn variant-filled-primary">
-							<iconify-icon icon={technologyIcons[codeLink.icon]} />
-						</a>
+						{#if deployment.link}
+							<Tooltip content={technologyIcons[deployment.icon].title}>
+								<a href={deployment.link} target="_blank" class="btn variant-filled-primary">
+									<iconify-icon icon={technologyIcons[deployment.icon].icon} />
+								</a>
+							</Tooltip>
+						{/if}
+						{#if codeLink.link}
+							<Tooltip content={technologyIcons[codeLink.icon].title}>
+								<a href={codeLink.link} target="_blank" class="ml-2 btn variant-filled-primary">
+									<iconify-icon icon={technologyIcons[codeLink.icon].icon} />
+								</a>
+							</Tooltip>
+						{/if}
 					</div>
 					<div class="grid items-center justify-start grid-flow-col gap-1">
-						<p>Stack:</p>
 						{#each technologyStack as icon}
-							<iconify-icon icon={technologyIcons[icon]} />
+							<Tooltip content={technologyIcons[icon].title}>
+								<iconify-icon icon={technologyIcons[icon].icon} />
+							</Tooltip>
 						{/each}
 					</div>
 				</div>
