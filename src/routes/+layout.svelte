ac7491b10cadf8f4352d5cbfacacc2477e8eb676
Jawad Ahmed <jawadahmedtaj@gmail.com>
2023-09-26 11:35:27 +0500

    Updated for mobile

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,14 @@
 <script>
 	import '../app.postcss';
-	import { AppShell, AppBar, LightSwitch } from '@skeletonlabs/skeleton';
+	import {
+		AppShell,
+		AppBar,
+		LightSwitch,
+		initializeStores,
+		getDrawerStore,
+		Drawer
+	} from '@skeletonlabs/skeleton';
+	initializeStores();
 
 	// Floating UI for Popups
 	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
@@ -36,6 +44,20 @@
 				circle.style.top = `${e.clientY - height / 2}px`;
 			}, 20);
 		});
+
+		animate(
+			'.drawerButton',
+			{
+				y: [0, 5]
+			},
+			{
+				duration: 0.5,
+				repeat: Infinity,
+				easing: 'ease-out',
+				direction: 'alternate-reverse'
+			}
+		);
+
 	});
 
 	const handleAnchorClick = (event) => {
@@ -43,15 +65,57 @@
 		const link = event.currentTarget;
 		const anchorId = new URL(link.href).hash.replace('#', '');
 		const anchor = document.getElementById(anchorId);
+		drawerStore.close();
 		anchor.scrollIntoView({
 			behavior: 'smooth',
 			block: 'start'
 		});
 	};
+
+	const drawerSettings = {
+		id: 'drawer',
+		bgDrawer: 'bg-surface-900',
+		bgBackdrop: 'bg-gradient-to-tr from-indigo-500/50 via-purple-500/50 to-pink-500/50',
+		width: 'w-full',
+		height: 'h-10',
+		padding: 'p-2',
+		rounded: 'rounded-xl',
+		position: 'bottom'
+	};
+
+	const drawerStore = getDrawerStore();
+
+	const openDrawer = () => {
+		drawerStore.open(drawerSettings);
+	};
 </script>
 
 <div class="hidden circle special md:block" />
 
+<svelte:head>
+	<title>Jawad Ahmed's portfolio</title>
+</svelte:head>
+
+<Drawer>
+	<div class="grid items-center grid-flow-col">
+		<a href="#work" on:click={handleAnchorClick} class="p-2 badge variant-filled special-dot"
+			>{$t('common.work')}</a
+		>
+		<a href="#about" on:click={handleAnchorClick} class="p-2 badge variant-filled special-dot"
+			>{$t('common.about')}</a
+		>
+		<a href="#contact" on:click={handleAnchorClick} class="p-2 badge variant-filled special-dot"
+			>{$t('common.contact')}</a
+		>
+	</div>
+</Drawer>
+
+<button
+	class="absolute z-10 block -translate-x-1/2 bottom-2 btn-icon variant-filled-surface md:hidden left-1/2 btn-icon-sm drawerButton"
+	on:click={openDrawer}
+>
+	<iconify-icon icon="mdi:arrow-up" class="text-2xl"/>
+</button>
 <!-- App Shell -->
 <AppShell>
 	<svelte:fragment slot="pageHeader">
@@ -63,19 +127,24 @@
 		>
 			<svelte:fragment slot="lead">
 				<a href="/" class="special-dot">
-					<strong class="text-xl uppercase special-dot"> Jawad A. </strong>
+					<strong class="text-xl uppercase special-dot">Jawad A.</strong>
 				</a>
 			</svelte:fragment>
 			<svelte:fragment slot="trail">
-				<a href="#work" on:click={handleAnchorClick} class="p-2 badge variant-filled special-dot"
-					>{$t('common.work')}</a
-				>
-				<a href="#about" on:click={handleAnchorClick} class="p-2 badge variant-filled special-dot"
-					>{$t('common.about')}</a
-				>
-				<a href="#contact" on:click={handleAnchorClick} class="p-2 badge variant-filled special-dot"
-					>{$t('common.contact')}</a
-				>
+				<div class="hidden md:inline-block">
+					<a href="#work" on:click={handleAnchorClick} class="p-2 badge variant-filled special-dot"
+						>{$t('common.work')}</a
+					>
+					<a href="#about" on:click={handleAnchorClick} class="p-2 badge variant-filled special-dot"
+						>{$t('common.about')}</a
+					>
+					<a
+						href="#contact"
+						on:click={handleAnchorClick}
+						class="p-2 badge variant-filled special-dot">{$t('common.contact')}</a
+					>
+				</div>
+
 				<LightSwitch />
 				{#if $locale === 'en'}
 					<button
